eb9b4353e047ef93938563dbf41d3875f0f2501c
apavlin <apavlin@yandex.ru>
2016-11-20 22:36:54 +0100

    task done

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,11 +1,18 @@
 //= ../../bower_components/jquery/dist/jquery.min.js
 
 $(document).ready(function() {
+  // Выводит блок с надписью и убирает заголовки со страницы
+var taskComleted = function() {
+  $('.taskComleted').css('display', 'block');
+  $('.courses .article__header, .article__subHeader').css('display', 'none');
+}
 
+var arrCourses = [];
 //  При отметке чекбокса блок закрашивается и исчезает через 1с
 var hiding = function() {
   var trigger = $('.course__looked_text');
     trigger.click(function() {
+      $(this).children('.course__looked_checkbox').css('background-image', 'url(/img/checkedbox.png');
       $(this).parent().prev().css({
         'background':'#edf5e3',
         'color': '#a4ac9b',
@@ -16,16 +23,15 @@ var hiding = function() {
         'background':'#e7efdb'
       });
       $(this).closest('.course').delay(500).fadeOut(500);
-      });
-  }
-// Выводит блок с надписью и убирает заголовки со страницы
-var taskComleted = function() {
-  $('.taskComleted').css('display', 'block');
-  $('.courses .article__header, .article__subHeader').css('display', 'none');
-}
 
-// если все блоки исчезли вывести надпись про выполненное задание
-if ( $(".course:visible").length != 0) { hiding() }
-  else { taskComleted() };
+      arrCourses = $(".course").filter(":visible");
+      if(arrCourses.length-1 == 0){
+        setTimeout(taskComleted, 1000)
+      }
+
+    });
+  }
+
+  hiding();
 
 });
